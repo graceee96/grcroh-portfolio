df8b5304600db141f59fad13e6e81aa4663b31ef
Grace Roh <grace@BIGMINE.attlocal.net>
2023-05-14 18:36:47 -0700

    create App.js and add CSS

--- src/App.js
+++ src/App.js
@@ -1,1 +1,22 @@
 import React from "react";
+import './App.css';
+
+//import components
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+export default function App() {
+    const renderPage = () => {};
+    
+    return (
+        <div id="content">
+            <Header />
+
+            <div id="section" className="p-4">
+                {renderPage()}
+            </div>
+
+            <Footer />
+        </div>
+    )
+}
