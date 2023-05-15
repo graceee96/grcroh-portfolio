7c59bf974a42eec9fc97dfe4d26a2c9a5e2c75f8
Grace Roh <grace@BIGMINE.attlocal.net>
2023-05-14 18:47:14 -0700

    add styling to header and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,26 @@
 import React from 'react';
 
+const styles = {
+    links: {
+        width: '45%'
+    },
+    text: {
+        color: '#E28B75',
+        textDecoration: 'none',
+        fontSize: '1.5vw',
+        fontWeight: 500,
+    }
+}
+
 export default function Footer() {
     return (
         <div className="d-flex justify-content-between pt-2">
-            <div className="d-flex justify-content-around">
-                <p><Link to="https://github.com/graceee96">GitHub</Link></p>
-                <p><Link to="https://www.linkedin.com/in/grcroh/">LinkedIn</Link></p>
-                <p>coming soon...</p>
+            <div className="d-flex justify-content-around" style={styles.links}>
+                <p><Link to="https://github.com/graceee96" style={styles.text}>GitHub</Link></p>
+                <p><Link to="https://www.linkedin.com/in/grcroh/" style={styles.text}>LinkedIn</Link></p>
+                <p style={styles.text}>coming soon...</p>
             </div>
-            <p class="mx-3">© Made with 💛 in 2023</p>
+            <p class="mx-3" style={styles.text}>© Made with 💛 in 2023</p>
         </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Navigation from './Navigation'
 
+const styles = {
+    h1: {
+        fontWeight: 600,
+        fontSize: '4vw',
+        marginBottom: '-0.5vw',
+    }
+}
+
 export default function Header() {
     return (
         <div className="navbar d-flex align-items-end">
-            <h1 className="mx-3">Grace Roh.</h1>
+            <h1 className="mx-3" style={styles.h1}>Grace Roh.</h1>
             <Navigation />
         </div>
     )
