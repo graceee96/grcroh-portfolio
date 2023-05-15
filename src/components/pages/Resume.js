ff27c34fda75de893cf04f8e9250d439b9e10503
Grace Roh <grace@BIGMINE.attlocal.net>
2023-05-14 18:57:02 -0700

    add styling to resume component

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,25 +1,42 @@
 import React from 'react';
 
+const styles = {
+    link: {
+        color: '#EE6352',
+        width: '45%',
+    },
+    skillList: {
+        width: '55%'
+    },
+    listHeader: {
+        fontSize: '3vw',
+        borderBottom: '2px solid #575555'
+    },
+    skills: {
+        fontSize: '2.5vw',
+    }
+}
+
 export default function Resume() {
     return (
         <section id="resume" className="h-100">
             <h2 className="section-title mb-4">Resume.</h2>
 
             <div className="d-flex justify-content-between">
-                <h3><Link id="resume-link" to="https://drive.google.com/file/d/1yX_Mcdt6li2MwKCx4Hd1EAf9AymzYeCQ/view?usp=sharing">View resume here ↗</Link></h3>
+                <h3><Link to="https://drive.google.com/file/d/1yX_Mcdt6li2MwKCx4Hd1EAf9AymzYeCQ/view?usp=sharing" style={styles.link}>View resume here ↗</Link></h3>
 
-                <div id="skill-list">
+                <div style={styles.skillList}>
                     <div className="mb-4">
-                        <h4 className="pb-2">front end proficiencies</h4>
-                        <p className="pt-2">HTML, CSS, JavaScript, JQUERY, Bootstrap, Bulma, Handlebars.js, React.js</p>
+                        <h4 className="pb-2" style={styles.listHeader}>front end proficiencies</h4>
+                        <p className="pt-2" style={styles.skills}>HTML, CSS, JavaScript, JQUERY, Bootstrap, Bulma, Handlebars.js, React.js</p>
                     </div>
                     <div className="mb-4">
-                        <h4 className="pb-2">back end proficiencies</h4>
-                        <p className="pt-2">Node.js, Express.js, MySQL, Sequelize, MongoDB, Mongoose, GraphQL</p>
+                        <h4 className="pb-2" style={styles.listHeader}>back end proficiencies</h4>
+                        <p className="pt-2" style={styles.skills}>Node.js, Express.js, MySQL, Sequelize, MongoDB, Mongoose, GraphQL</p>
                     </div>
                     <div className="mb-4">
-                        <h4 className="pb-2">other skills</h4>
-                        <p className="pt-2">Figma, Adobe Creative Suite</p>
+                        <h4 className="pb-2" style={styles.listHeader}>other skills</h4>
+                        <p className="pt-2" style={styles.skills}>Figma, Adobe Creative Suite</p>
                     </div>
                 </div>
             </div>
